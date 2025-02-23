4d88445274a5067044f87c33072b30e6d9cc081f
Marina Pelosi <maa.pelosi@gmail.com>
2025-02-23 17:15:18 -0500

    :building_construction: :alien: feat(pet list): structuring code, api and adding listing feature

--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 import { createApp } from 'vue';
-import App from './App.vue';
 import router from './router.js';
 
-createApp(App).use(router).mount('#app');
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import App from './App.vue';
+
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+//import { createVuetify } from 'vuetify'
+
+// export default createVuetify({
+//   icons: {
+//     defaultSet: 'mdi', // This is already the default value - only for display purposes
+//   },
+// })
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+          mdi,
+        },
+      },
+  })
+
+const application = createApp(App).use(router);
+application.use(vuetify);
+application.mount('#app');
